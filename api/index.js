8a1619d07a6641cb2573615156072d6ddaa3e51e
AnnaShiba <102438674+AnnaShiba@users.noreply.github.com>
2024-10-06 19:13:00 -0400

    added mongoose library

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 const app = express();
+const Employee = require('../models/employee');
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
---
+++ models/employee.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const employeeSchema = new mongoose.Schema({
+    first_name: { type: String, required: true, maxlength: 100 },
+    last_name: { type: String, required: true, maxlength: 50 },
+    email: { type: String, required: true, unique: true, maxlength: 50 },
+    position: { type: String, required: true, unique: true, maxlength: 50 },
+    salary: { type: Number, required: true },
+    date_of_joining: { type: Date, required: true },
+    department: { type: String, enum: ['Engineering', 'Product', 'Sales'], maxlength: 25 },
+});
+
+export default mongoose.model('Employee', employeeSchema);
